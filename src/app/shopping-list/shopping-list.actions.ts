c30375a3bd31f59ad363b98453c78f634c721e22
miha-9111 <miha-9111@yandex.ru>
2021-11-24 22:30:44 +0300

    NgRx -> added - shopping-list.actions.ts

---
+++ src/app/shopping-list/shopping-list.actions.ts
@@ -0,0 +1,9 @@
+import { IngredientModel } from "../shared/ingredient.model";
+import { Action } from "@ngrx/store";
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+
+export class AddIngredient implements Action {
+  readonly type = ADD_INGREDIENT;
+  payload: IngredientModel;
+}
