a303f843c2d97e3c317b9337ce70772c42039f71
miha-9111 <miha-9111@yandex.ru>
2021-11-24 22:26:22 +0300

    NgRx -> Adding Logic to the Reducer - shopping-list.reducer.ts

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,5 +1,7 @@
 import { IngredientModel } from "../shared/ingredient.model";
 
+import { Action } from "@ngrx/store";
+
 const initialState = {
   ingredients: [
     new IngredientModel('Apples', 5),
@@ -7,4 +9,12 @@ const initialState = {
   ]
 };
 
-export function shoppingListReducer(state = initialState, action) {}
+export function shoppingListReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action]
+      };
+  }
+}
