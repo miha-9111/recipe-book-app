bbf5b7ab8ee755d7a5d678dc57ac34fea5f3fad6
miha-9111 <miha-9111@yandex.ru>
2021-12-09 22:42:38 +0300

    some fixes -> auth.service, auth.component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,7 @@
 import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import {Observable, Subscription} from "rxjs";
-import { Router } from "@angular/router";
+import { Subscription} from "rxjs";
 
-import { AuthResponseData, AuthService } from "./auth.service";
 import { AlertComponent } from "../shared/alert/alert/alert.component";
 import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 
@@ -25,9 +23,7 @@ export class AuthComponent implements OnInit, OnDestroy {
   private closeSub: Subscription;
   private storeSub: Subscription;
 
-  constructor(private authService: AuthService,
-              private router: Router,
-              private componentFactoryResolver: ComponentFactoryResolver,
+  constructor(private componentFactoryResolver: ComponentFactoryResolver,
               private store: Store<fromApp.AppState>) {}
 
   ngOnInit() {
