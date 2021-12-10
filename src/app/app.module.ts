40508fa06a7f233e0044f4be5ff59393b8bc4ce9
miha-9111 <miha-9111@yandex.ru>
2021-12-10 04:11:18 +0300

    NgRx -> Adding -> The Router Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import * as fromApp from "./store/app.reducer";
 import { EffectsModule } from "@ngrx/effects";
 import { AuthEffects } from "./auth/store/auth.effects";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -30,6 +31,7 @@ import { environment } from "../environments/environment";
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument( {logOnly: environment.production} ),
+    StoreRouterConnectingModule.forRoot(),
   ],
   bootstrap: [AppComponent]
 })
