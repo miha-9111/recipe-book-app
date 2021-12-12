d98c7d4746097392220157401cb312babb56fffa
miha-9111 <miha-9111@yandex.ru>
2021-12-12 23:53:22 +0300

    NgRx -> Fetching Recipes & Using the Resolver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { StoreModule } from "@ngrx/store";
 import * as fromApp from "./store/app.reducer";
 import { EffectsModule } from "@ngrx/effects";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { RecipeEffects } from "./recipes/store/recipe.effects";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
@@ -29,7 +30,7 @@ import { environment } from "../environments/environment";
     SharedModule,
     CoreModule,
     StoreModule.forRoot(fromApp.appReducer),
-    EffectsModule.forRoot([AuthEffects]),
+    EffectsModule.forRoot([AuthEffects, RecipeEffects]),
     StoreDevtoolsModule.instrument( {logOnly: environment.production} ),
     StoreRouterConnectingModule.forRoot(),
   ],
--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
+import { take } from "rxjs/operators";
+import { Store } from "@ngrx/store";
+import { Actions, ofType } from "@ngrx/effects";
+
 import { RecipeModel } from "./recipe.model";
-import { DataStorageService } from "../shared/data-storage.service";
-import {RecipeService} from "./recipe.service";
+import * as fromApp from "../store/app.reducer";
+import * as RecipesActions from "../recipes/store/recipe.actions";
 
 @Injectable({providedIn: "root"})
 export class RecipesResolverService implements Resolve<RecipeModel[]>{
-  constructor(private dataStorageService: DataStorageService,
-              private recipeService: RecipeService) {}
+  constructor(private store: Store<fromApp.AppState>,
+              private actions$: Actions) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const recipes = this.recipeService.getRecipes();
-    if (recipes.length === 0) {
-      return this.dataStorageService.fetchRecipes();
-    } else {
-      return recipes;
-    }
+    // const recipes = this.recipeService.getRecipes();
+    // if (recipes.length === 0) {
+    //   return this.dataStorageService.fetchRecipes();
+    // } else {
+    //   return recipes;
+    // }
+    this.store.dispatch(new RecipesActions.FetchRecipes());
+    return this.actions$.pipe(
+      ofType(RecipesActions.SET_RECIPES),
+      take(1)
+    );
   }
 }
