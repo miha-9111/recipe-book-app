bcf11c3a508bbedf85fd93c76b6895e280ac7658
miha-9111 <miha-9111@yandex.ru>
2021-12-14 22:19:55 +0300

    NgRx -> Fix Auth Redirect -> auth.effects and auth.actions

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -33,7 +33,8 @@ const handleAuthentication = (
     email: email,
     userId: userId,
     token: token,
-    expirationDate: expirationDate
+    expirationDate: expirationDate,
+    redirect: true
   });
 };
 
@@ -121,8 +122,10 @@ export class AuthEffects {
   @Effect({dispatch: false})
   authRedirect = this.actions$.pipe(
     ofType(AuthActions.AUTHENTICATE_SUCCESS),
-    tap(() => {
-      this.router.navigate(['/']);
+    tap((authSuccessActions: AuthActions.AuthenticateSuccess) => {
+      if (authSuccessActions.payload.redirect) {
+        this.router.navigate(['/']);
+      }
     })
   );
 
@@ -156,7 +159,8 @@ export class AuthEffects {
             email: loadedUser.email,
             userId: loadedUser.id,
             token: loadedUser.token,
-            expirationDate: new Date(userData._tokenExpirationDate)
+            expirationDate: new Date(userData._tokenExpirationDate),
+            redirect: false
           });
         // const expirationDuration =
         //   new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
