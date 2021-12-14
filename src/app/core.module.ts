3d7f4b8110f74221124cb4a82851a5d1f66c2093
miha-9111 <miha-9111@yandex.ru>
2021-12-15 00:57:10 +0300

    Deletting -> recipe-service and some fixes

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from "@angular/core";
-import { RecipeService } from "./recipes/recipe.service";
 import { AuthInterceptorService } from "./auth/auth.interceptor.service";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 
 @NgModule({
   providers: [
-    RecipeService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
